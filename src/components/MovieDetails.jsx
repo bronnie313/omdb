504ad33b69f37d31cb77009ed321df54e8958eea
Bisaso Ronald <bronnie313@gmail.com>
2024-03-22 23:21:33 +0300

    Add Error handling, state managment and Feedback

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -3,9 +3,10 @@ import { NavLink, useParams } from 'react-router-dom'
 import { Icon } from '@iconify/react'
 
 const MovieDetails = () => {
+  const [isError, setIsError] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const { imdbID } = useParams()
   const [selectedMovie, setSelectedMovie] = useState(null)
-  console.log(selectedMovie)
 
   useEffect(() => {
     const fetchDetails = async () => {
@@ -13,37 +14,56 @@ const MovieDetails = () => {
         const response = await fetch(
           `https://www.omdbapi.com/?i=${imdbID}&apikey=2fa5119d`
         )
+        console.log(response)
+        if (!response.ok && !selectedMovie) {
+          setIsError(true)
+          setIsLoading(false)
+          return
+        }
         const specData = await response.json()
         setSelectedMovie(specData)
       } catch (error) {
+        setIsError(true)
         console.error(error)
       }
+      setIsLoading(false)
     }
     fetchDetails()
   }, [imdbID])
+
+  if (isLoading) {
+    return (
+      <div style={{ marginTop: '9rem', textAlign: 'center' }}>
+        <Icon icon="eos-icons:bubble-loading" fontSize={'2rem'} />
+        <div style={{ marginTop: '1rem' }}></div>
+        <p>loading</p>
+      </div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div style={{ fontSize: '25px', marginTop: '8rem', textAlign: 'center' }}>
+        There is an error ...
+      </div>
+    )
+  }
 
   return (
     <div className="container-2">
       <NavLink to="/">
         <Icon className="icon" icon="icon-park-solid:back" />
       </NavLink>
-      {selectedMovie && (
-        <div className="details">
-          <div className="details-wrapper">
-            <img src={selectedMovie.Poster} alt={selectedMovie.Title} />
-            <div className="info">
-              <h2 style={{ fontFamily: 'cursive' }}>{selectedMovie.Title}</h2>
-              <p>{selectedMovie.Plot}</p>
-              <h4>{selectedMovie.Genre}</h4>
-            </div>
+      <div className="details">
+        <div className="details-wrapper">
+          <img src={selectedMovie.Poster} alt={selectedMovie.Title} />
+          <div className="info">
+            <h2 style={{ fontFamily: 'cursive' }}>{selectedMovie.Title}</h2>
+            <p>{selectedMovie.Plot}</p>
+            <h4>{selectedMovie.Genre}</h4>
           </div>
         </div>
-      )}
-      {!selectedMovie && (
-        <div style={{ marginTop: '10rem' }}>
-          <Icon icon="eos-icons:bubble-loading" fontSize={'2rem'} />
-        </div>
-      )}
+      </div>
     </div>
   )
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,25 +2,49 @@ import { useEffect, useState } from 'react'
 import MovieCard from './MovieCard'
 import SearchBar from './SearchBar'
 import { NavLink } from 'react-router-dom'
+import { Icon } from '@iconify/react'
 
-const url = 'https://www.omdbapi.com/?s=drunken&apikey=2fa5119d'
+const url = 'https://www.omdbapi.com/?s=horses&apikey=2fa5119d'
 
 const MovieList = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(url)
+        if (!response.ok) {
+          setIsLoading(false)
+          setIsError(true)
+          return
+        }
         const data = await response.json()
         setMovies(data.Search)
       } catch (error) {
+        setIsError(true)
         console.error(error)
       }
+      setIsLoading(false)
     }
 
     fetchData()
   }, [])
+
+  if (isLoading) {
+    return (
+      <div style={{ marginTop: '9rem', textAlign: 'center' }}>
+        <Icon icon="eos-icons:bubble-loading" fontSize={'2rem'} />
+        <div style={{ marginTop: '1rem' }}></div>
+        <p>loading</p>
+      </div>
+    )
+  }
+
+  if (isError) {
+    return <h3>There was an error .. </h3>
+  }
 
   return (
     <div className="container">
